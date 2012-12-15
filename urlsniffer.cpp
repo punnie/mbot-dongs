17f81efa71860b159d1535aa1e13b47d8611ef36
ticklemynausea <emailoftheyear@gmail.com>
2012-12-15 17:27:47 +0000

    now urlsniffer uses EasyCurl

--- urlsniffer.cpp
+++ urlsniffer.cpp
@@ -25,7 +25,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #define SEND_TEXT s->script.send_text
 
 #define MAX_URL_LENGTH 1024
-
+#define MAX_URL_DETECT 3
 struct sniffing_channel_type {
   String* channel;
   bool tellall; // sniff all links or just text/html
@@ -57,28 +57,12 @@ List * urlsniffer_list;
 // Utils
 ///////////////
 
-#include <stdio.h>
-#include <ctype.h>
-#include <string.h>
 
-
-#define MAX_URL_DT 3
-#define USERAGENT_STR "Opera/12.80 (Windows NT 5.1; U; en) Presto/2.10.289 Version/12.02"
-#define ISPRINT_LOCALE1 "pt_PT.UTF-8"
-#define ISPRINT_LOCALE2 "pt_PT.ISO-8859-1"
 #include <iostream>
 #include <string>
 #include <sstream>
-#include <locale>
-#include <algorithm>
-#include "curl/curl.h"
+#include "easycurl/easycurl.h"
 
-bool is_not_printable(char c) {
-  std::locale l1(ISPRINT_LOCALE1);
-  //std::locale l2(ISPRINT_LOCALE2);
-  //return !(std::isprint(c,l1) or std::isprint(c,l2));
-  return !std::isprint(c,l1);
-}
 
 char* ircstrip(char* src, char* dst) {
   int dst_i = 0;
@@ -118,7 +102,7 @@ int geturls (char* src, char dst[][MSG_SIZE]) {
     if ((strncmp(temp, "http://", 7) == 0) 
     or  (strncmp(temp, "https://", 8) == 0) 
     or  (strncmp(temp, "www.", 4) == 0)) {
-      if (count < MAX_URL_DT) {
+      if (count < MAX_URL_DETECT) {
        strcpy(dst[count], temp);
        count++;
       } else {
@@ -140,6 +124,7 @@ static void urlsniffer_conf (NetServer *, c_char);
 static void urlsniffer_stop (void);
 static void urlsniffer_start (void);
 extern size_t decode_html_entities_utf8(char *dest, const char *src);
+
 extern "C" {
 
 EXPORT struct Module::module_type module = {
@@ -153,253 +138,6 @@ EXPORT struct Module::module_type module = {
 
 }
 
-///////////
-// libcurl
-//////////
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <algorithm>
-#include "curl/curl.h"
-
-#define MAX_TITLE_LEN 256
-
-// Limit buffer size to 500 KB
-#define MAX_DL_SIZE (500*1024)
-
-// tell fail?
-class MyCurlRequest {
-  
-  public:
-    int ok;
-    std::string effective_url;
-    std::string content_type;
-    std::string content_length;
-    std::string response_code;
-    std::string redirect_count;
-    std::string buffer; 
-    std::string curlstatus;
-    std::string htmltitle;
-
-    MyCurlRequest() {
-    }
-
-    MyCurlRequest(CURLcode result) {
-      this->ok = 0;
-      this->curlstatus = curl_easy_strerror(result);
-    }
-
-    MyCurlRequest(CURLcode result, char* effective_url, string content_type, double content_length, long response_code, long redirect_count, std::string buffer) {
-
-      std::ostringstream oss;
-      std::string s;
-      
-      oss.precision(20);
-      this->curlstatus = curl_easy_strerror(result);
-      this->ok = 1;
-
-      oss << effective_url;
-      this->effective_url = oss.str();
-      oss.str("");
-
-      oss << content_type;
-      s = oss.str();
-      remove_if(s.begin(), s.end(), is_not_printable);
-      this->content_type = s;
-      oss.str("");
-      
-      oss << content_length;
-      this->content_length = oss.str();
-      oss.str("");
-
-      oss << response_code;
-      this->response_code = oss.str();
-      oss.str("");
-
-      oss << redirect_count;
-      this->redirect_count = oss.str();
-      oss.str("");
-      
-      oss << buffer;
-      s = oss.str();
-      remove_if(s.begin(), s.end(), is_not_printable);
-      this->buffer = s;
-      oss.str("");
-
-      this->htmltitle = "N/A";
-    }
-
-    void printme() {
-      ostringstream oss;
-      oss << "url title: " << this->htmltitle << " (Content-Type " << this->content_type 
-          <<  ", Content-Length " << this->content_length << ", HTTP Status " << this->response_code 
-          << ", " << this->redirect_count << " redirects. " << this->buffer.length() << " bytes used.)";
-      std::cout << oss.str() << std::endl;
-    
-    }
-        
-    bool is_html() {
-      return this->content_type.compare(0, 9, "text/html") == 0;
-    }
-    
-    void loadHTMLTitle() {
-      char buff[MSG_SIZE];
-      try {
-        size_t pos1 = this->buffer.find("<title>");
-        size_t pos2 = this->buffer.find("</title>");
-        if (pos1 == -1)
-          pos1 = this->buffer.find("<TITLE>");
-        
-        if (pos2 == -1)
-          pos2 = this->buffer.find("</TITLE>");
-
-        size_t tlen = pos2-(pos1+7);
-
-        if (tlen > MAX_TITLE_LEN) {
-          std::cout << pos1 << " " << pos2 << std::endl;
-          this->htmltitle = "Failed to retrieve :(";
-          return;
-        }
-        std::string title = this->buffer.substr(pos1+7, tlen);
-
-        
-        size_t wpos1 = title.find_first_not_of(" \t\n\r");
-        size_t wpos2 = title.find_last_not_of(" \t\n\r");
-        
-        this->htmltitle = title.substr(wpos1, wpos2-wpos1+1);
-        strncpy(buff, this->htmltitle.c_str(), MSG_SIZE);
-        decode_html_entities_utf8(buff,(char*)NULL);
-        this->htmltitle = buff;
-      } catch (...) {
-        this->htmltitle = "Failed to retrieve. :(";
-      }
-    }
-};
-
-class MyCurl
-{
-  private:
-
-   static int writer(char *data, size_t size, size_t nmemb, std::string *buffer_in)
-    {
-      // Is there anything in the buffer?
-      if (buffer_in != NULL)
-      {
-        // Append the data to the buffer
-        buffer_in->append(data, size * nmemb);
-        bufferTotal += size * nmemb;
-        
-        //Stop if max size exceeded by returning 0
-        if (bufferTotal < MAX_DL_SIZE) {
-          return size * nmemb;
-        } else {
-          return 0;
-        }
-      }
-
-      return 0;
-    }
-   
-  public:
-    static int bufferTotal;
-    static MyCurlRequest get(std::string url, bool getbody) {
-      const char* c = url.c_str();
-      return get(c, getbody);
-    }
-
-    static MyCurlRequest get(const char* url, bool getbody)
-    {
-
-      char* effective_url;
-      char* content_type;
-      char content_type_stripped[MSG_SIZE];
-      double content_length;
-      long response_code;
-      long redirect_count;
-
-      CURL *curl;
-      CURLcode result;
-      
-      // Create our curl handle
-      curl = curl_easy_init();
-      //curl = curl_global_init(CURL_GLOBAL_ALL)
-      char errorBuffer[CURL_ERROR_SIZE];
-      
-      // Write all expected data in here
-      std::string buffer;
-
-      if (curl)
-      {
-        curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errorBuffer);
-        curl_easy_setopt(curl, CURLOPT_URL, url);
-        curl_easy_setopt(curl, CURLOPT_USERAGENT, USERAGENT_STR);
-        curl_easy_setopt(curl, CURLOPT_TIMEOUT, 5); //I'm not sure how this works
-        curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 10);
-        curl_easy_setopt(curl, CURLOPT_HEADER, 0);
-        curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
-        //curl_easy_setopt(curl, CURLOPT_RANGE, DL_RANGE);
-        curl_easy_setopt(curl, CURLOPT_ENCODING, "identity");
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
-       if (getbody) {
-          curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
-          curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, MyCurl::writer);
-          curl_easy_setopt(curl, CURLOPT_NOBODY, 0);
-        } else {
-          curl_easy_setopt(curl, CURLOPT_WRITEHEADER, &buffer);
-          curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, MyCurl::writer);
-          curl_easy_setopt(curl, CURLOPT_NOBODY, 1);
-        }
-        // Attempt to retrieve the remote page
-        result = curl_easy_perform(curl);
-
-        // Along with the request get some information
-        curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_URL, &effective_url);
-        curl_easy_getinfo(curl, CURLINFO_CONTENT_TYPE, &content_type);
-
-        curl_easy_getinfo(curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD, &content_length);
-        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
-        curl_easy_getinfo(curl, CURLINFO_REDIRECT_COUNT, &redirect_count);
-      }
-
-      MyCurlRequest req;
-      //std::cout << buffer <<std::endl;
-      if(result == CURLE_OK) {
-        req = MyCurlRequest(result, effective_url, content_type,
-          content_length, response_code, redirect_count, buffer);
-      } else {
-        req = MyCurlRequest(result);
-      }
-
-
-      // Always cleanup
-      curl_easy_cleanup(curl);
-      return req;
-    }
-      // EAZY :-)
-    static MyCurlRequest easyget(std::string url) {
-      return easyget(url.c_str());
-    }
-
-    static MyCurlRequest easyget(const char* url) {
-      bufferTotal = 0;
-      MyCurlRequest h = MyCurl::get(url, false);
-      if (h.ok == 1) {
-        std::transform(h.content_type.begin(), h.content_type.end(), h.content_type.begin(), ::tolower);
-        if (h.is_html()) {
-          MyCurlRequest b = MyCurl::get(url, true); //if text/html then we are going to do the full req
-          if ((b.ok) and (b.content_type.compare(0, 9, "text/html")==0)) {
-            b.loadHTMLTitle();
-          }
-          return b; 
-        } else {
-          return h;
-        }
-      } else {
-        return h; 
-      }
-    }
-};
-int MyCurl::bufferTotal;
 
 //check to see if a certain channel is in the mod's sniff list
 bool urlsniffer_type::sniffing_channel(c_char channel) {
@@ -419,10 +157,7 @@ bool urlsniffer_type::telling_fail_on_channel(c_char channel) {
   sniffing_channel_type *s;
   this->channels->rewind ();
   while ((s = (sniffing_channel_type *)this->channels->next()) != NULL)
-  {
-   
-    printf("tellfail is %d on %s\n", (int)s->tellfail, s->channel->getstr());
-    
+  { 
     if (*(s->channel) == channel)
       return s->tellfail;
   }
@@ -444,6 +179,8 @@ bool urlsniffer_type::telling_all_on_channel(c_char channel) {
 /////////////
 // commands
 /////////////
+
+// N/A
 
 /////////////////////
 // events
@@ -470,7 +207,7 @@ urlsniffer_event (NetServer *s)
   if (!urlsniffer->sniffing_channel(CMD[2]))
     return;
 	
-  char buff[MAX_URL_DT][MSG_SIZE];
+  char buff[MAX_URL_DETECT][MSG_SIZE];
   int count;
   char cmdcopy[MSG_SIZE];
   strcpy(cmdcopy, CMD[3]);
@@ -492,30 +229,33 @@ urlsniffer_event (NetServer *s)
     std::ostringstream oss;
     std::string url = buff[i];
     std::string nick = SOURCE;
-    MyCurlRequest x = MyCurl::easyget(url);
+    EasyCurl e(url);
     
-    if (x.ok == 1) {
-
-       oss << nick << "'s link title: " << x.htmltitle << " (";
-       if (x.response_code != "200") {
-         oss << "HTTP " << x.response_code << ", ";
+    if (e.requestWentOk == 1) {
+       if (e.isHtml) {
+         oss << nick << "'s link title: " << e.html_title << " (";
+       } else {
+         oss << nick << "'s link title: N/A (";
        }
-         oss << x.content_type;
-       if (x.content_length == "-1") {
+       if (e.response_code != "200") {
+         oss << "HTTP " << e.response_code << ", ";
+       }
+       oss << e.response_content_type;
+       if (e.response_content_length == "-1") {
          oss << ")";
        } else {
-         oss << ", " << x.content_length << " bytes)";
+         oss << ", " << e.response_content_length << " bytes)";
        }
     } else {
-      oss << nick << "'s link sucks. (" << x.curlstatus << ")";
+      oss << nick << "'s link sucks. (" << e.error_message << ")";
     }
     
 
     
-    if ((x.ok == 0) && !urlsniffer->telling_fail_on_channel(CMD[2]))
+    if ((!e.requestWentOk) && !urlsniffer->telling_fail_on_channel(CMD[2]))
       return;
       
-    if (!x.is_html() && !urlsniffer->telling_all_on_channel(CMD[2]))
+    if (!e.isHtml && !urlsniffer->telling_all_on_channel(CMD[2]))
       return;
     
     
