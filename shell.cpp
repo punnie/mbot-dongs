cf5c214a3d8df3f74d8e20acd20750ede914a469
ticklemynausea <emailoftheyear@gmail.com>
2013-01-25 23:56:19 +0000

    It works! And allows command injection.. argh

--- shell.cpp
+++ shell.cpp
@@ -87,6 +87,9 @@ List * shell_list;
 #include <ctype.h>
 #include <string.h>
 
+#include <string>
+#include <sstream>
+using namespace std;
 
 ///////////////
 // prototypes
@@ -127,6 +130,46 @@ EXPORT struct Module::module_type module = {
 // commands
 /////////////
 
+// http://stackoverflow.com/questions/3418231/c-replace-part-of-a-string-with-another-string
+
+bool replace(std::string& str, const std::string& from, const std::string& to) {
+    size_t start_pos = str.find(from);
+    if(start_pos == std::string::npos)
+        return false;
+    str.replace(start_pos, from.length(), to);
+    return true;
+}
+
+//builds the shell command based on user input
+void parse_command(char* buffer, command_type* command, char* params) {
+
+  string params_s(params), 
+    command_s(command->command->getstr()), 
+    temp;
+  stringstream ss(params_s);
+  
+  int n = 1;
+  while (ss >> temp) {
+    stringstream temp_ss("");
+    temp_ss << "{" << n++ << "}";
+     
+    //cout << "a: " << temp << endl;
+    //cout << "b: " << temp_ss.str() << endl;
+    //cout << "c: " << command_s << endl;
+    replace(command_s, temp_ss.str(), temp);
+    //cout << "d: " << command_s << endl;
+  }
+  
+  strncpy(buffer, command_s.c_str(), COMMAND_SIZE);
+  
+
+
+  //deprecated
+  //cout << "a: "<< command->trigger->getstr() << endl;
+  //sprintf(buffer, command->command->getstr(), "~", "l");
+  //cout << "b: \""<< params << "\"" << endl;
+}
+
 //returns the command for a given trigger
 command_type* trigger2command(char* trigger, List* command_list) {
   command_type *command;
@@ -134,7 +177,6 @@ command_type* trigger2command(char* trigger, List* command_list) {
   
   while ((command = (command_type *)command_list->next ()) != NULL) {
     if (*(command->trigger) == trigger) {      
-      cout << *(command->trigger) << endl;
       return command;
     }
   }
@@ -153,7 +195,7 @@ shell_cmd (NetServer *s)
     return;
   }
 
-  strsplit (CMD[3], BUF, 2);
+  strsplit (CMD[3], BUF, 1);
 
   command_type* command = trigger2command(BUF[0], shell->commands);
   
@@ -163,23 +205,35 @@ shell_cmd (NetServer *s)
     
   } else {
   
-    char* cmdline = command->command->getstr();
+    
+    char cmdline[COMMAND_SIZE];
+    parse_command(cmdline, command, BUF[1]);
+    
+    //debug
+    cout << "Command: " << cmdline << endl;
+        
     int lines_sent = 0;
     char buffer[RESULT_LINE_SIZE];
     FILE* p;
   
     if ((p = popen(cmdline, "r")) == NULL) {
+    
       SEND_TEXT(DEST, "OMG HELP SOMETHING IS WRONG WITH 13%s", BUF[0]);
       return;
+      
     }
     
     while(!feof(p)) {
 
-      if ((fgets(buffer, RESULT_LINE_SIZE, p) == NULL) 
-      || (lines_sent >= RESULT_LINE_MAX)) {
+      if (fgets(buffer, RESULT_LINE_SIZE, p) == NULL) {
+        break;
+      }
+      
+      if (lines_sent >= RESULT_LINE_MAX) {
         SEND_TEXT(DEST, "command output stopped at %d lines.", RESULT_LINE_MAX);
         break;
       }
+
       
       SEND_TEXT(DEST, "%s", buffer);
       lines_sent++;
@@ -235,7 +289,6 @@ static void
 shell_conf (NetServer *s, c_char bufread)
 {
 
-  cout << ">> " << bufread << endl;
   char buf[5][MSG_SIZE+1];
   strsplit (bufread, buf, 4);
  
@@ -263,7 +316,7 @@ shell_conf (NetServer *s, c_char bufread)
   ));
   //s->script.events.add ((void *)shell_event);
   
-  cout << "binding " << buf[1] << endl;
+  cout << " * binding " << buf[1] << " to " << buf[4] << endl;
   s->script.cmd_bind (shell_cmd, cmdlevel, buf[1], module.mod, HELP_shell);
 }
 
@@ -271,22 +324,19 @@ shell_conf (NetServer *s, c_char bufread)
 static void
 shell_stop (void)
 {
-  cout << "A" << endl;
   shell_type *shell;
+  command_type *command;
   shell_list->rewind ();
-  cout << "B" << endl;
-  while ((shell = (shell_type *)shell_list->next ()) != NULL)
-  {
-  
-    cout << "B1" << endl;
+  while ((shell = (shell_type *)shell_list->next ()) != NULL) {
     shell->s->script.events.del ((void *)shell_event);
-  cout << "B2" << endl;
-//    delete shell->commands;
-  cout << "B3" << endl;
-  delete shell;
-  cout << "B4" << endl;
+    
+    shell->commands->rewind();
+    while ((command = (command_type*)shell->commands->next ()) != NULL) {
+      delete command;
+    }
+    delete shell;
+    
   }
-  cout << "C" << endl;
   delete shell_list;
 }
 
